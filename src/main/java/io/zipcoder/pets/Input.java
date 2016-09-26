c51f33a34998fd4dbd960e778cec212e3bd0e6b1
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-26 18:33:45 -0400

    added input class, test class added

---
+++ src/main/java/io/zipcoder/pets/Input.java
@@ -0,0 +1,18 @@
+package io.zipcoder.pets;
+
+import java.util.Scanner;
+
+/**
+ * Created by jonathanleczner on 9/26/16.
+ */
+public class Input {
+    Scanner s = new Scanner(System.in);
+
+    public Input() {
+
+    }
+
+    public void promptUser() {
+        System.out.println();
+    }
+}
---
+++ src/test/java/io/zipcoder/pets/DogTest.java
@@ -0,0 +1,15 @@
+package io.zipcoder.pets;
+
+import org.junit.Test;
+
+/**
+ * Created by jonathanleczner on 9/26/16.
+ */
+public class DogTest {
+    Dog d = new Dog("Fido");
+
+    @Test
+    public void speakTest() {
+        // needed?
+    }
+}
