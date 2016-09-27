86ea10a284300ab7c76132a9434921855d1e9deb
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-27 11:52:15 -0400

    added main class, added methods in input for running the program

---
+++ src/main/java/io/zipcoder/pets/App.java
@@ -0,0 +1,12 @@
+package io.zipcoder.pets;
+
+/**
+ * Created by jonathanleczner on 9/27/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        Input i = new Input();
+        i.runProgram();
+    }
+
+}
--- src/main/java/io/zipcoder/pets/Input.java
+++ src/main/java/io/zipcoder/pets/Input.java
@@ -1,18 +1,73 @@
 package io.zipcoder.pets;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Created by jonathanleczner on 9/26/16.
  */
 public class Input {
-    Scanner s = new Scanner(System.in);
+    Scanner s;
+    List<String> inputs;
 
     public Input() {
-
+        s = new Scanner(System.in);
+        inputs = new ArrayList<>();
     }
 
-    public void promptUser() {
-        System.out.println();
+    public void runProgram() {
+        int amount = getHowManyPets();
+        getWhatKind(amount);
+        getPetNames(amount);
+        printPets();
+    }
+
+    public String getInput() {
+        return s.next();
+    }
+
+    private void askHowManyPets() {
+        System.out.println("How many pets do you have?");
+    }
+
+    private int getHowManyPets() {
+        askHowManyPets();
+        inputs.add(getInput());
+        return Integer.valueOf(inputs.get(0));
+    }
+
+    private void askWhatKind() {
+        System.out.println("What kind of pets are they?");
+    }
+
+    private void getWhatKind(int amount) {
+        askWhatKind();
+        int petsSpecified = 0;
+        while (petsSpecified < amount) {
+            inputs.add(getInput());
+            petsSpecified++;
+        }
+    }
+
+    private void askPetNames() {
+        System.out.println("What are their names?");
+    }
+
+    private void getPetNames(int amount) {
+        askPetNames();
+        int petsNamed = 0;
+        while (petsNamed < amount) {
+            inputs.add(getInput());
+            petsNamed++;
+        }
+    }
+
+    private void printPets() {
+        int amount = Integer.valueOf(inputs.get(0));
+        System.out.println(amount + " pets");
+        for (int i = 1; i <= amount; i++) {
+            System.out.println(i + " " + inputs.get(i) + ": " + inputs.get(i + amount));
+        }
     }
 }
